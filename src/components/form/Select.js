90078b5d6b275631321e9d1df94ed5a9b25c2d65
Adriano Misina <adriano.misina@gmail.com>
2022-06-18 20:48:48 -0300

    get and post api methods

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import styles from './Select.module.css'
 
-const Select = ({text, name, options, handleOnChange, value}) => {
+function Select({ text, name, options, handleOnChange, value }) {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <select name={name} id={name}>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ''}
+      >
         <option>Selecione uma opção</option>
+        {options.map((options) => (
+          <option value={options.id} key={options.id}>
+            {options.name}
+          </option>
+        ))}
       </select>
     </div>
   )
